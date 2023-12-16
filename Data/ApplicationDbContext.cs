b60c224dbb54e10481ad1284b5c529b70b9821b6
PKubis <piotrkubis1989@gmail.com>
2023-12-16 21:50:07 +0100

    Prawie gotowe

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using _4Ballers.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Linq; // Dodaj tę dyrektywę using
+
+namespace _4Ballers.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
+    }
+}
